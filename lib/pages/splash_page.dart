439a21dd1d6bc0c0dc88b1e436a916ce1c6fec14
Monam1083 <monammajeed1083@gmail.com>
2026-08-05 15:15:20 +0500

    update

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -6,7 +6,6 @@ import 'package:movie_application/models/config.dart';
 
 class SplashPage extends StatefulWidget {
   final VoidCallback onInitializationComplete;
-
   const SplashPage({super.key, required this.onInitializationComplete});
 
   @override
