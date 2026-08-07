b9317a6ee453dae23df9b4e0b6514ad2e5852229
Monam1083 <monammajeed1083@gmail.com>
2026-08-07 21:10:42 +0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
-//Packages
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-//Pages
 import './pages/splash_page.dart';
 import './pages/main_page.dart';
 
