cff479371b249468eb5ae36852e4b45dd89e91e6
Monam1083 <monammajeed1083@gmail.com>
2026-08-07 21:10:19 +0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,31 @@
+//Packages
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:movie_application/pages/main_page.dart';
-import 'package:movie_application/pages/splash_page.dart';
+
+//Pages
+import './pages/splash_page.dart';
+import './pages/main_page.dart';
 
 void main() {
   runApp(
     SplashPage(
       key: UniqueKey(),
-      onInitializationComplete: () {
-        runApp(ProviderScope(child: const MyApp()));
-      },
+      onInitializationComplete: () => runApp(ProviderScope(child: MyApp())),
     ),
   );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Flicked",
-      debugShowCheckedModeBanner: false,
-      initialRoute: "home",
-      routes: {"home": (BuildContext context) => const MainPage()},
+      title: 'Flickd',
+      initialRoute: 'home',
+      routes: {'home': (BuildContext _context) => MainPage()},
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
     );
   }
 }
