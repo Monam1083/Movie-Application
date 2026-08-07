28970f7e7b82cd464dc5bfacefce9d6f629728ed
Monam1083 <monammajeed1083@gmail.com>
2026-08-07 20:41:37 +0500

    update

--- lib/models/config.dart
+++ lib/models/config.dart
@@ -1,11 +1,1 @@
-import 'package:flutter/material.dart';
 
-class AppConfig {
-  // ignore: non_constant_identifier_names
-  final String BASE_API_URl;
-  // ignore: non_constant_identifier_names
-  final String BASE_IMAGE_URl;
-  // ignore: non_constant_identifier_names
-  final String API_KEY;
-  AppConfig({required this.BASE_API_URl, this.BASE_IMAGE_URl, this.API_KEY});
-}
