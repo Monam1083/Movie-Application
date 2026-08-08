e975771e4d74bee05f9cf8aa5762da53def18093
Monam1083 <monammajeed1083@gmail.com>
2026-08-08 20:57:27 +0500

    uodate

--- lib/services/movie_screen.dart
+++ lib/services/movie_screen.dart
@@ -5,9 +5,7 @@ import '../models/movie.dart';
 
 class MovieService {
   final GetIt getIt = GetIt.instance;
-
   late HTTPService _http;
-
   MovieService() {
     _http = getIt.get<HTTPService>();
   }
